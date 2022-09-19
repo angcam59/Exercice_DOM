16adc719306ef5f996c82dea92a06d9f097bc482
angcam59 <guerra.angelo@orange.fr>
2022-09-19 16:28:06 +0200

    Essai de récupération du DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("root").innerHTML = "papa est à la maison";
+
+/*
+const titre = document.getElementsByTagName("h1").innerHTML = "nouveau titre";
+*/
+
+const titre = document.getElementsByClassName("titre").innerHTML = "nouveau titre";
+console.log(titre);
